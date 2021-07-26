c7016858e3ba6c2bf1ef952a19fcb7b19a2cdfa9
Aman Jaiswal <amanjaiswal159.aj@icloud.com>
2021-07-26 21:52:59 +0530

    configured the Procfile and server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@
 
 var express = require("express");
 var bodyParser = require('body-parser');
-var db = require('./db');
 const app = express();
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
@@ -44,13 +43,11 @@ app.get("/", function (req, res) {
 app.post("/adddata", function (req, res, next) {
     console.log(2);
     const item = new Item(req.body);
-    // console.log(item);
     console.log(item);
     item.save();
     res.send("Success")
 
 });
-
 
 app.get("/alldata", function (req, res) {
     console.log(3);
@@ -59,68 +56,6 @@ app.get("/alldata", function (req, res) {
         res.render("table", { userData: foundItems });
     });
 })
-
-// ============================MYSQL========================================================
-
-// Send data with single image to the database.
-
-app.post("/add", function (req, res, next) {
-    console.log(4);
-
-    var departmentName = req.body.department;
-    var productName = req.body.product;
-    var quantity = req.body.quantity;
-    var voucherName = req.body.voucher;
-
-    var sql = `INSERT into vbu (department,product,quantity, voucher) VALUES ("${departmentName}", "${productName}", "${quantity}", "${voucherName}")`;
-    db.query(sql, function (err, result) {
-        if (err) {
-            res.status(500).send({ error: 'Unable to send data to the database' });
-        }
-        res.send("Upload success");
-    });
-});
-
-
-//  ---- READ ----
-// Access all the Data from the databse.
-
-app.get("/all", function (req, res, next) {
-    console.log(5);
-    var sql = "SELECT * FROM vbu";
-    db.query(sql, function (err, rows, fields) {
-        if (err) {
-
-            res.status(500).send({ error: 'Unable to fetch data from the database' });
-        }
-        // res.json(rows);
-        res.render("table", { userData: rows })
-    })
-    // res.send("Success!!!")
-})
-
-
-
-app.get("/custom", function (req, res, next) {
-    console.log(6);
-    // var sql = "SELECT * FROM vbu WHERE product='${departmentName}'";
-    var sql = "SELECT * FROM vbu WHERE product= 'pen'";
-    db.query(sql, function (err, rows, fields) {
-        if (err) {
-
-            res.status(500).send({ error: 'Unable to fetch data from the database' });
-        }
-        // res.json(rows);
-        res.render("table", { userData: rows })
-    })
-    // res.send("Success!!!")
-})
-
-//  ---- UPDATE ----
-
-
-//  ---- DELETE ----
-
 
 // CRUD Operations end.
 
